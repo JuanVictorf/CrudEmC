c6bdf4b8e1d35f1dec01b06469ffb6e1cfd060bb
Juan_Victor <estudantejuan0@gmail.com>
2022-09-24 16:51:59 -0300

    Código feito

--- main.c
+++ main.c
@@ -13,44 +13,102 @@ typedef struct Agenda{
 	
 } Agenda;
 
+int tamanhoAgenda = 2;
 Agenda vetAgenda[10];
-
 
 int cadastrarAgenda(Agenda *vetAgenda){
 	
 	int i;
-	for(i = 0; i < 2; i++){
+	for(i = 0; i < tamanhoAgenda; i++){
 		printf("Digite o nome da pessoa: \n");
 		scanf("%s", &vetAgenda[i].nome);
-		//setbuf(stdin, NULL);
-    	//scanf("%99[^\n]", &vetAgenda[i].nome);
 		printf("Digite o telefone da pessoa: \n");
 		scanf("%s", &vetAgenda[i].telefone);
-		//setbuf(stdin, NULL);
-    	//scanf("%99[^\n]", &vetAgenda[i].telefone);
 		printf("Digite a empresa da pessoa: \n");
 		scanf("%s", &vetAgenda[i].empresa);
-		//setbuf(stdin, NULL);
-    	//scanf("%99[^\n]", &vetAgenda[i].empresa);
 	}
 }
 
 int consultarAgenda(Agenda *vetAgenda){
-	//Agenda vetAgenda[10];
 	int i;
-	int retornoString;
+	char opcao[100];
 	
-	for(i = 0; i < 2; i++){
-		//printf("\nPosicao i: %s\n", vetAgenda[i].nome);
-		retornoString = strcmp(vetAgenda[i].nome, "");
-		//printf("%i", retornoString);
-		if(retornoString != 0){
+	printf("Digite Todo para listar agenda completa ou Nome: \n");
+	scanf("%s", opcao);
+	
+	if(strcmp(opcao, "Todo") == 0){
+		for(i = 0; i < tamanhoAgenda; i++){
 			printf("Nome da pessoa: %s\n", vetAgenda[i].nome);
 			printf("Telefone da pessoa: %s\n", vetAgenda[i].telefone);
 			printf("Empresa da pessoa: %s\n", vetAgenda[i].empresa);
 		}
 	}
 	
+	if(strcmp(opcao, "Nome") == 0){
+		char nomeConsulta[50];
+		printf("Digite o nome que deseja alterar: \n");
+		scanf("%s", &nomeConsulta);
+		for(i = 0; i < tamanhoAgenda; i++){
+			if(strcmp(vetAgenda[i].nome, nomeConsulta) == 0){
+				printf("Nome da pessoa: %s\n", vetAgenda[i].nome);
+				printf("Telefone da pessoa: %s\n", vetAgenda[i].telefone);
+				printf("Empresa da pessoa: %s\n", vetAgenda[i].empresa);
+			}
+		}
+	}
+}
+
+int alteraAgenda(Agenda *vetAgenda){
+	char nomeConsulta[50];
+	printf("Digite o nome que deseja alterar: \n");
+	scanf("%s", &nomeConsulta);
+	int retornoString;
+	
+	int i;
+	for(i = 0; i < tamanhoAgenda; i++){
+		retornoString = strcmp(vetAgenda[i].nome, nomeConsulta);
+		if(retornoString == 0){
+			char alteracao[50];
+			printf("Registro existe, o que deseja alterar?\n");
+			printf("Nome: %s\n", vetAgenda[i].nome);
+			printf("Telefone: %s\n", vetAgenda[i].telefone);
+			printf("Empresa: %s\n", vetAgenda[i].empresa);
+			scanf("%s", &alteracao);
+			if(strcmp(alteracao, "Nome") == 0){
+				printf("Digite o novo nome: \n");
+				scanf("%s", &vetAgenda[i].nome);
+			} else if(strcmp(alteracao, "Telefone") == 0){
+				printf("Digite o novo telefone: \n");
+				scanf("%s", &vetAgenda[i].telefone);
+			} else if(strcmp(alteracao, "Empresa") == 0){
+				printf("Digite o novo nome da empresa: \n");
+				scanf("%s", &vetAgenda[i].empresa);
+			} else{
+				printf("Nao existe essa opcao.\n");
+			}
+		}
+	}
+	
+}
+
+int deletaAgenda(Agenda *vetAgenda){
+	char nomeConsulta[50];
+	printf("Deseja deletar qual registro, insira o nome: ");
+	scanf("%s", &nomeConsulta);
+	
+	int retornoString;
+	int i, j;
+	for(i = 0; i < tamanhoAgenda; i++){
+		retornoString = strcmp(vetAgenda[i].nome, nomeConsulta);
+		if(retornoString == 0){
+			for(j = i; j < tamanhoAgenda - 1; j++){
+				vetAgenda[j] = vetAgenda[j + 1];
+				// Posicao que ele achou recebe a próxima posicao do vetor
+			}
+		} 
+	}
+	tamanhoAgenda--;
+	printf("Registro excluido!\n");
 }
 
 
@@ -85,10 +143,10 @@ int main(int argc, char *argv[]) {
 			consultarAgenda(&vetAgenda);
 			break;
 		case 3:
-			printf("");
+			alteraAgenda(&vetAgenda);
 			break;
 		case 4:
-			printf("");
+			deletaAgenda(&vetAgenda);
 			break;
 		case 5:
 			break;
